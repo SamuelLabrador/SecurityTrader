eb8387d531fa98762200540b9fafa63d795caf32
SamuelLabrador <sam.labrador@yahoo.com>
2021-01-28 23:45:13 -0800

    Initial websocket service. Left comments for possible multiplexing expansion.

--- Webapp/webapp/src/app/app.component.ts
+++ Webapp/webapp/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { WebSocketService } from './services/websocket.service';
+import {WebSocketMessage} from "./fetch/api";
+import {MessageType} from "./org/constants";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +13,6 @@ export class AppComponent {
   title = 'webapp';
 
   constructor (private apiService: ApiService) {}
-
 
   ngOnInit() {
     this.getStatus();
--- Webapp/webapp/src/app/services/websocket.service.ts
+++ Webapp/webapp/src/app/services/websocket.service.ts
@@ -1,4 +1,3 @@
-import { R3ExpressionFactoryMetadata } from '@angular/compiler/src/render3/r3_factory';
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { ConfigurationService } from './configuration.service';
@@ -7,12 +6,39 @@ import { WebSocketMessage } from '../fetch/api';
 @Injectable({
   providedIn: 'root'
 })
-export class WebsocketService {
+export class WebSocketService {
   webSocket: WebSocketSubject<unknown>;
 
-  constructor(private configService: ConfigurationService) { 
+  constructor(private configService: ConfigurationService) {
     this.webSocket = webSocket(configService.wsUrl);
 
-    // TODO: Research and implement ws logic
+    // This is temporary... In the future, will most likely need
+    // to multiplex. Refer to this guide on rxjs websocket:
+    // https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
+    this.webSocket.subscribe(
+      msg => this.msgCallback(msg),
+      err => console.log(err),
+      () => console.log('connection closed.')
+    );
+  }
+
+  /**
+   * @remarks
+   * Sends a websocket message to the server
+   *
+   * @param message The websocket message sent to server.
+   */
+  sendMessage(message: WebSocketMessage): void {
+    this.webSocket.next(message);
+  }
+
+  /**
+   * @remarks
+   * This callback method is TEMPORARY and processes websocket messages
+   *
+   * @param msg message received from the websocket
+   */
+  private msgCallback(msg: unknown) {
+    console.log('message received from websocket: ' + msg);
   }
 }
