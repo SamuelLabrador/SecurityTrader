a70e1c5fa7aa7aa4df7f7da2665f5c5fe19dab6d
SamuelLabrador <sam.labrador@yahoo.com>
2021-01-30 16:22:26 -0800

    Removing unused imports. Simplifying websocket service.

--- Webapp/webapp/src/app/services/websocket.service.ts
+++ Webapp/webapp/src/app/services/websocket.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { ConfigurationService } from './configuration.service';
-import { WebSocketMessage } from '../fetch/api';
 
 @Injectable({
   providedIn: 'root'
@@ -12,33 +11,7 @@ export class WebSocketService {
   constructor(private configService: ConfigurationService) {
     this.webSocket = webSocket(configService.wsUrl);
 
-    // This is temporary... In the future, will most likely need
-    // to multiplex. Refer to this guide on rxjs websocket:
+    // For handling websocket messages, checkout this guide:
     // https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
-    this.webSocket.subscribe(
-      msg => this.msgCallback(msg),
-      err => console.log(err),
-      () => console.log('connection closed.')
-    );
-  }
-
-  /**
-   * @remarks
-   * Sends a websocket message to the server
-   *
-   * @param message The websocket message sent to server.
-   */
-  sendMessage(message: WebSocketMessage): void {
-    this.webSocket.next(message);
-  }
-
-  /**
-   * @remarks
-   * This callback method is TEMPORARY and processes websocket messages
-   *
-   * @param msg message received from the websocket
-   */
-  private msgCallback(msg: unknown) {
-    console.log('message received from websocket: ' + msg);
   }
 }
