4bf84e56239c19646b941455136fcef4f25af90c
ene_07 <miguelandrade49@gmail.com>
2021-02-22 16:46:02 -0800

    basic structure for authenticatorControllerSpec for now will be a todo

--- Server/test/controllers/AuthenticatorControllerSpec.scala
+++ Server/test/controllers/AuthenticatorControllerSpec.scala
@@ -8,14 +8,14 @@ import play.api.test.{FakeRequest, Helpers, Injecting}
 
 import scala.concurrent.Future
 
-class AuthenticatorControllerSpec extends PlaySpec
+/*class AuthenticatorControllerSpec extends PlaySpec
   with GuiceOneAppPerTest
   with Injecting
   with BeforeAndAfterAll{
   // TODO understand if it is better to make a test method for each method
   // TODO perhaps methods are not allowed
 
-  /*  "createUser" should {
+    "createUser" should {
       "Should return the value of the created user " in {
         val mockDataBaseInterface = mock[]
         val controller             = new AuthenticatorController( , Helpers.stubControllerComponents())
@@ -23,6 +23,6 @@ class AuthenticatorControllerSpec extends PlaySpec
         val bodyText: String       = contentAsString(result)
         bodyText mustBe "ok"
       }
-    }*/
+    }
 
-}
+}*/
