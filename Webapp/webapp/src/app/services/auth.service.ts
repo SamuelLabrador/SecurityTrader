38a708ea26cdb7064d11ea33fa15daa5df92fc54
SamuelLabrador <sam.labrador@yahoo.com>
2021-02-27 17:29:02 -0800

    adding the auth service

---
+++ Webapp/webapp/src/app/services/auth.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+import {ModelsRestPlayer} from "../fetch/api";
+import {ApiService} from "./api.service";
+
+export interface SecurityTraderToken {
+  exp: number;
+  iat: number;
+  id: string;
+  email: string;
+  username: string;
+  isMod: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private token: string = '';
+  private tokenData: SecurityTraderToken | undefined = undefined;
+
+  constructor() { }
+
+  /**
+   * Gets the token string
+   */
+  getToken(): String {
+    return this.token;
+  }
+
+  /**
+   * Gets the parsed token data
+   */
+  getTokenData(): SecurityTraderToken | undefined {
+    return this.tokenData;
+  }
+
+  /**
+   * Gets the parsed user data from the token data
+   */
+  getUserData(): ModelsRestPlayer | undefined {
+    const result = this.tokenData ? this.tokenData as ModelsRestPlayer : undefined;
+    return result;
+  }
+
+  /**
+   * This method should be invoked upon a successful login.
+   * ie:
+   *
+   *  sampleLogin(email, password) {
+   *    const response = await this.apiService.login(email, password);
+   *    this.authService.setToken(response.token);
+   *  }
+   *
+   * @param token The token returned by the server
+   */
+  setToken(token: string) {
+    try {
+      const tokenInfo = jwt_decode(token) as SecurityTraderToken;
+      this.tokenData = tokenInfo;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
