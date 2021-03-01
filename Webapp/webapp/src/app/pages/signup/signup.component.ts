cfca44019992b368b7baa02efc0d63b383058df4
ene_07 <miguelandrade49@gmail.com>
2021-02-28 17:48:23 -0800

    testing things

--- Webapp/webapp/src/app/pages/signup/signup.component.ts
+++ Webapp/webapp/src/app/pages/signup/signup.component.ts
@@ -3,10 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.sass']
+  styleUrls: ['./signup.component.sass', '../../app.component.sass']
 })
 export class SignupComponent implements OnInit {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   constructor() { }
 
   ngOnInit(): void {
